08cdf7608576bb7087d5d5e551a167de7385c72b
riccardo-dagna <dagnariccardo@gmail.com>
2024-02-27 17:05:07 +0100

    Added special attacks file, generic file for now

---
+++ components/special_attacks.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Tuple, Optional
+import random
+
+from components.base_component import BaseComponent
+
+if TYPE_CHECKING:
+    from entity import Actor
+
+class SpecialAttacks(BaseComponent):
+    parent: Actor
+
+    def __init__(self) -> None:
+        pass
+
+
