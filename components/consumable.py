00a62a797412fb24099f5b747521b847902fa3ed
riccardo-dagna <dagnariccardo@gmail.com>
2024-05-16 16:48:24 +0200

    Added fear scroll class, and modified confusion and stun scroll to reflect new changes in development

--- components/consumable.py
+++ components/consumable.py
@@ -38,9 +38,6 @@ class Consumable(BaseComponent):
 
 
 class ConfusionConsumable(Consumable):
-    def __init__(self, number_of_turns: int):
-        self.number_of_turns = number_of_turns
-
     def get_action(self, consumer: Actor) -> SingleRangedAttackHandler:
         self.engine.message_log.add_message("Select a target location.", color.needs_target)
         return SingleRangedAttackHandler(
@@ -64,7 +61,34 @@ class ConfusionConsumable(Consumable):
             color.status_effect_applied,
         )
         target.status.dict_condition_afflicted["flag_confusion"] = True
-        target.status.turns_passed = 0
+        target.status.dict_turns_passed["confusion"] = 0
+        self.consume()
+
+class FearConsumable(Consumable):
+    def get_action(self, consumer: Actor) -> SingleRangedAttackHandler:
+        self.engine.message_log.add_message("Select a target location.", color.needs_target)
+        return SingleRangedAttackHandler(
+            self.engine,
+            callback=lambda xy: actions.ItemAction(consumer, self.parent, xy),
+        )
+
+    def activate(self, action: actions.ItemAction) -> None:
+        consumer = action.entity
+        target = action.target_actor
+
+        if not self.engine.game_map.visible[action.target_xy]:
+            raise Impossible("You cannot target an area that you cannot see.")
+        if not target:
+            raise Impossible("You must select an enemy to target.")
+        if target is consumer:
+            raise Impossible("You cannot confuse yourself!")
+
+        self.engine.message_log.add_message(
+            f"The face of the {target.name} look scared, as it starts to run away from you!",
+            color.status_effect_applied,
+        )
+        target.status.dict_condition_afflicted["flag_fear"] = True
+        target.status.dict_turns_passed["fear"] = 0
         self.consume()
 
 
@@ -169,10 +193,7 @@ class LightningDamageConsumable(Consumable):
         else:
             raise Impossible("No enemy is close enough to strike.")
 
-class StunConsumable(Consumable):
-    def __init__(self, number_of_turns: int) -> None:
-        self.number_of_turns = number_of_turns
-    
+class StunConsumable(Consumable):    
     def get_action(self, consumer: actions.Actor) -> SingleRangedAttackHandler:
         self.engine.message_log.add_message("Select a target location.", color.needs_target)
         return SingleRangedAttackHandler(
@@ -196,5 +217,5 @@ class StunConsumable(Consumable):
             color.status_effect_applied,
         )
         target.status.dict_condition_afflicted["flag_stun"] = True
-        target.status.turns_passed = 0
+        target.status.dict_turns_passed["stun"] = 0
         self.consume()
