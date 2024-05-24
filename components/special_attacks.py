caf1098c3d125a8560b3890e55165b5d1007477c
riccardo.dagna719 <riccardo.dagna719@edu.unito.it>
2024-05-24 08:40:32 +0200

    Added some dict with all the special attacks chosen

--- components/special_attacks.py
+++ components/special_attacks.py
@@ -10,8 +10,19 @@ if TYPE_CHECKING:
 
 class SpecialAttacks(BaseComponent):
     parent: Actor
+    turns_to_recharge: int = 3
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self, 
+                 flag_ingest: bool = False, flag_percentile: bool = False, flag_stats_drain: bool = False, flag_rot: bool = False, flag_steal: bool = False, flag_dispel: bool = False,
+                 values_percentile: int = 0, value_strenght_drain: int = 0, value_agility_drain: int = 0,
+                 status_ingest: bool = False,
+                ) -> None:
+        self.dict_special_attacks_flag = dict(ingest = flag_ingest, percentile = flag_percentile, stats_drain = flag_stats_drain, rot = flag_rot, steal = flag_steal, dispel = flag_dispel,)
+        self.dict_special_attack_values = dict(percentile = values_percentile, strenght_drain = value_strenght_drain, agility_drain = value_agility_drain,)
+        self.dict_special_attack_status = dict(ingest = status_ingest,)
+        turns_recharge: int = 0
+
+    
+
 
 
