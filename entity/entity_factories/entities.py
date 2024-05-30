2e51d98a77c3bc6296dec4a2d2fbdfb98d396c41
riccardo-dagna <dagnariccardo@gmail.com>
2024-05-30 11:16:02 +0200

    Added special attack object to all entities, and added new entity to test special attacks

--- entity/entity_factories/entities.py
+++ entity/entity_factories/entities.py
@@ -1,9 +1,10 @@
-from components.ai import HostileEnemy
+from components.ai import HostileMeeleeEnemy, SpecialEnemy
 from components.damageinfo import DamageInfo
 from components.equipment import Equipment
 from components.fighter import Fighter
 from components.inventory import Inventory
 from components.level import Level
+from components.special_attacks import SpecialAttacks
 from components.status import Status
 from entity.entity import Actor, Chest
 
@@ -11,24 +12,32 @@ player = Actor(
     char="@",
     color=(255, 255, 255),
     name="Player",
-    ai_cls=HostileEnemy,
+    ai_cls=HostileMeeleeEnemy,
     equipment=Equipment(),
     fighter=Fighter(hp=99, base_defense=10, base_power=2),
     inventory=Inventory(capacity=26),
     level=Level(level_up_base=200),
     status=Status(),
     damage_info=DamageInfo(),
+    special_attacks=SpecialAttacks()
 )
 
 
 #This sections contains the enemies
 orc = Actor(char="o", color=(63, 127, 63), name="Orc",
-    ai_cls=HostileEnemy, equipment=Equipment(), fighter=Fighter(hp=10, base_defense=0, base_power=3),
+    ai_cls=HostileMeeleeEnemy, equipment=Equipment(), fighter=Fighter(hp=10, base_defense=0, base_power=3),
     inventory=Inventory(capacity=0), level=Level(xp_given=35), status=Status(), damage_info=DamageInfo(),
+    special_attacks=SpecialAttacks(),
 )
 troll = Actor(char="T", color=(0, 127, 0), name="Troll", 
-    ai_cls=HostileEnemy, equipment=Equipment(), fighter=Fighter(hp=16, base_defense=1, base_power=4),
+    ai_cls=HostileMeeleeEnemy, equipment=Equipment(), fighter=Fighter(hp=16, base_defense=1, base_power=4),
     inventory=Inventory(capacity=0), level=Level(xp_given=100), status=Status(), damage_info=DamageInfo(),
+    special_attacks=SpecialAttacks(),
+)
+vampire = Actor(char="v", color=(63,127, 63), name="Vampire",
+    ai_cls=SpecialEnemy, equipment=Equipment(), fighter=Fighter(hp=15, base_defense=2, base_power=2),
+    inventory=Inventory(capacity=0), level=Level(xp_given=200), status=Status(), damage_info=DamageInfo(),
+    special_attacks=SpecialAttacks(),
 )
 
 
