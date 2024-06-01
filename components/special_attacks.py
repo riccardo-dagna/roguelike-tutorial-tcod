a99aa6b256f241da6c140b73a577bb0e0db78137
riccardo.dagna719 <riccardo.dagna719@edu.unito.it>
2024-06-01 16:31:09 +0200

    Added corrosion special attacks and effect of corrosion

--- components/special_attacks.py
+++ components/special_attacks.py
@@ -13,18 +13,21 @@ class SpecialAttacks(BaseComponent):
     turns_to_recharge: int = 5
 
     def __init__(self, 
-                 flag_ingest: bool = False, flag_percentile: bool = False, flag_stats_drain: bool = False, flag_rot: bool = False, flag_steal: bool = False, flag_dispel: bool = False,
+                 flag_ingest: bool = False, flag_percentile: bool = False, flag_stats_drain: bool = False, flag_rot: bool = False, flag_steal: bool = False, flag_dispel: bool = False, flag_corrosion: bool = False,
                  values_percentile: int = 0, value_strenght_drain: int = 0, value_agility_drain: int = 0,
-                 damage_ingest: int = 0, damage_stat_drain: int = 0, damage_rot: int = 0, damage_steal: int = 0, damage_dispel: int = 0,
-                 status_ingest: bool = False, status_rot: bool = False,
-                 immunity_ingest: bool = False, immunity_percentile: bool = False, immunity_stats_drain: bool = False, immunity_rot: bool = False, immunity_steal: bool = False, immunity_dispel: bool = False,
+                 damage_ingest: int = 0, damage_stat_drain: int = 0, damage_rot: int = 0, damage_steal: int = 0, damage_dispel: int = 0, damage_corrosion: int = 0,
+                 status_ingest: bool = False,
+                 immunity_ingest: bool = False, immunity_percentile: bool = False, immunity_stats_drain: bool = False, immunity_rot: bool = False, immunity_steal: bool = False, immunity_dispel: bool = False, immunity_corrosion: bool = False,
                 ) -> None:
-        self.dict_special_attacks_flag = dict(ingest = flag_ingest, percentile = flag_percentile, stats_drain = flag_stats_drain, rot = flag_rot, steal = flag_steal, dispel = flag_dispel,)
+        self.dict_special_attacks_flag = dict(ingest = flag_ingest, percentile = flag_percentile, stats_drain = flag_stats_drain, rot = flag_rot, steal = flag_steal, dispel = flag_dispel,
+                                              corrosion = flag_corrosion,)
         self.dict_special_attack_values = dict(percentile = values_percentile, strenght_drain = value_strenght_drain, agility_drain = value_agility_drain,)
-        self.dict_special_attack_damage = dict(ingest = damage_ingest, stats_drain = damage_stat_drain, rot = damage_rot, steal = damage_steal, dispel = damage_dispel,)
-        self.dict_special_attack_status = dict(ingest = status_ingest, rot = status_rot,)
-        self.dict_special_attack_immunity = dict(ingest = immunity_ingest, percentile = immunity_percentile, stats_drain = immunity_stats_drain, rot = immunity_rot, steal = immunity_steal, dispel = immunity_dispel,)
-        self.dict_turns_recharge = dict(ingest = 0, percentile = 0, stats_drain = 0, rot = 0, steal = 0, dispel = 0,)
+        self.dict_special_attack_damage = dict(ingest = damage_ingest, stats_drain = damage_stat_drain, rot = damage_rot, steal = damage_steal, dispel = damage_dispel, 
+                                               corrosion = damage_corrosion)
+        self.dict_special_attack_status = dict(ingest = status_ingest,)
+        self.dict_special_attack_immunity = dict(ingest = immunity_ingest, percentile = immunity_percentile, stats_drain = immunity_stats_drain, rot = 
+                                                 immunity_rot, steal = immunity_steal, dispel = immunity_dispel, corrosion = immunity_corrosion,)
+        self.dict_turns_recharge = dict(ingest = 0, percentile = 0, stats_drain = 0, rot = 0, steal = 0, dispel = 0, corrosion = 0,)
     
     @property
     def check_attack_ingest(self) -> bool:
@@ -49,6 +52,10 @@ class SpecialAttacks(BaseComponent):
     @property
     def check_attack_dispel(self) -> bool:
         return self.dict_special_attacks_flag["dispel"]
+    
+    @property
+    def check_attack_corrosion(self) -> bool:
+        return self.dict_special_attacks_flag["corrosion"]
 
     @property
     def check_turns_ingest(self) -> bool:
@@ -75,8 +82,12 @@ class SpecialAttacks(BaseComponent):
         return self.dict_turns_recharge["dispel"] >= (self.turns_to_recharge - 1) or self.dict_turns_recharge["dispel"] == 0
     
     @property
+    def check_turns_corrosion(self) -> bool:
+        return self.dict_turns_recharge["corrosion"] >= (self.turns_to_recharge - 1) or self.dict_turns_recharge["corrosion"] == 0
+    
+    @property
     def check_for_special_attack_ready(self) -> bool:
-        return (self.check_attack_ingest and self.check_turns_ingest) or (self.check_attack_percentile and self.check_turns_percentile) or (self.check_attack_stats and self.check_turns_stats_drain) or (self.check_attack_rot and self.check_turns_rot) or (self.check_attack_steal and self.check_turns_steal) or (self.check_attack_dispel and self.check_turns_dispel)
+        return (self.check_attack_ingest and self.check_turns_ingest) or (self.check_attack_percentile and self.check_turns_percentile) or (self.check_attack_stats and self.check_turns_stats_drain) or (self.check_attack_rot and self.check_turns_rot) or (self.check_attack_steal and self.check_turns_steal) or (self.check_attack_dispel and self.check_turns_dispel) or (self.check_attack_corrosion and self.check_turns_corrosion)
     
 
     def drain_stats_target(self, target: Actor) -> None:
@@ -121,13 +132,44 @@ class SpecialAttacks(BaseComponent):
             for item in target.inventory.items:
                 if not item_removed:
                     if item.is_organic and item.magic_item == False:
-                        if target.equipment.item_is_equipped(item):
-                            target.equipment.toggle_equip(item, False)
-                        target.inventory.items.remove(item)
+                        if item.equippable is not None:
+                            if item.damaged is False:
+                                item.damaged = True
+                            else:
+                                target.equipment.toggle_equip(item, False)
+                                target.inventory.items.remove(item)
+                        elif item.equippable is None:
+                            target.inventory.items.remove(item)
                         item_removed = True
 
             target.fighter.hp -= self.dict_special_attack_damage["rot"]
             self.engine.message_log.add_message(f"{target.name} is hit by a powerful spell, dealing {self.dict_special_attack_damage["rot"]}.")
+
+            if item_removed:
+                self.engine.message_log.add_message(f"{target.name} hears a rumor from it's backpack, like something rotting.")
+
+
+    def corrosion_damage(self, target: Actor) -> None:
+        if target.inventory.capacity == 0:
+            target.fighter.hp -= self.dict_special_attack_damage["corrosion"]*2
+            self.engine.message_log.add_message(f"{target.name} is hit by a powerful spell, and starts to corrode slowly.")
+        else:
+            item_removed = False
+            for item in target.inventory.items:
+                if not item_removed:
+                    if item.is_metallic and item.magic_item == False:
+                        if item.equippable is not None:
+                            if item.damaged is False:
+                                item.damaged = True
+                            else:
+                                target.equipment.toggle_equip(item, False)
+                                target.inventory.items.remove(item)
+                        elif item.equippable is None:
+                            target.inventory.items.remove(item)
+                        item_removed = True
+
+            target.fighter.hp -= self.dict_special_attack_damage["corrosion"]
+            self.engine.message_log.add_message(f"{target.name} is hit by a powerful spell, dealing {self.dict_special_attack_damage["corrosion"]}.")
 
             if item_removed:
                 self.engine.message_log.add_message(f"{target.name} hears a rumor from it's backpack, like something rotting.")
