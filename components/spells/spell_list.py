5e1e08fbff1aaa800c904d432c260ec43e5f6a9c
riccardo.dagna719 <riccardo.dagna719@edu.unito.it>
2024-08-22 18:12:38 +0200

    Added new status and cure spells

--- components/spells/spell_list.py
+++ components/spells/spell_list.py
@@ -11,16 +11,29 @@ from game_logic.engine import Engine
 
 class Fireball(Spell):
     def __init__(self, parent: Entity) -> None:
-        super().__init__(name="Fireball", damage=12, mana=1, max_range=0, radius=3, status=None, type="damage", parent=parent, handler=AreaRangedAttackHandler)
+        super().__init__(name="Fireball", damage=12, mana=1, radius=3, type="damage", parent=parent, handler=AreaRangedAttackHandler)
 
 class LightningBolt(Spell):
     def __init__(self, parent: Entity) -> None:
-        super().__init__(name="Lightning bolt", damage=20, mana=1, max_range=5, radius=0, status=None, type="damage", parent=parent, handler=None)
+        super().__init__(name="Lightning bolt", damage=20, mana=1, max_range=5, type="damage", parent=parent,)
 
 class IceDart(Spell):
     def __init__(self, parent: Entity) -> None:
-        super().__init__(name="Ice dart", damage=20, mana=1, max_range=0, radius=0, status=None, type="damage", parent=parent, handler=SingleRangedAttackHandler)
+        super().__init__(name="Ice dart", damage=20, mana=1, type="damage", parent=parent, handler=SingleRangedAttackHandler)
 
+class Confusion(Spell):
+    def __init__(self, parent: Entity) -> None:
+        super().__init__(name="Confusion", mana=1, status="confusion", type="status", parent=parent, handler=SingleRangedAttackHandler)
 
+class Fear(Spell):
+    def __init__(self, parent: Entity) -> None:
+        super().__init__(name="Fear", mana=1, status="fear", type="status", parent=parent, handler=SingleRangedAttackHandler)
 
+class Stun(Spell):
+    def __init__(self, parent: Entity) -> None:
+        super().__init__(name="Stun", mana=1, status="stun", type="status", parent=parent, handler=SingleRangedAttackHandler)
+
+class Heal(Spell):
+    def __init__(self, parent: Entity) -> None:
+        super().__init__(name="Heal", damage=10, mana=1, type="cure", parent=parent)
 
